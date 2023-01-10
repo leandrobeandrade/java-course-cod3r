b7919d5c3d5179c0d8265f49d35b858223d2ca83
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-01-09 23:41:34 -0300

    Java

---
+++ intermed/src/interfaces_foreach/Foreach.java
@@ -0,0 +1,34 @@
+package interfaces_foreach;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+		List<String> aprovados = Arrays.asList("Fulano", "Beltrano", "Ciclano");
+		
+		System.out.println("Forma tradicional...");
+		
+		for (String nome: aprovados) {
+			System.out.println(nome);
+		}
+		
+		System.out.println("\nLambda #1...");
+		aprovados.forEach(nome -> System.out.println(nome));
+		
+		System.out.println("\nMethod reference #1...");
+		aprovados.forEach(System.out::println);
+
+		System.out.println("\nLambda #2...");
+		aprovados.forEach(nome -> imprimir(nome));
+		
+		System.out.println("\nMethod reference #2...");
+		aprovados.forEach(Foreach::imprimir);
+	}
+	
+	static void imprimir(String nome) {
+		System.out.println("Nome: " + nome);
+	}
+
+}
