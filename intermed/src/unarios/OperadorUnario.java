1184677302b35aa70b7e72e3e938406c7f261823
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-01-14 00:12:23 -0300

    Java

---
+++ intermed/src/unarios/OperadorUnario.java
@@ -0,0 +1,19 @@
+package unarios;
+
+import java.util.function.UnaryOperator;
+
+public class OperadorUnario {
+
+	public static void main(String[] args) {
+		UnaryOperator<Integer> maisDois = n -> n + 2;
+		UnaryOperator<Integer> vezesDois = n -> n * 2;
+		UnaryOperator<Integer> aoQuadrado = n -> n * n;
+		
+		System.out.println(maisDois.andThen(vezesDois).andThen(aoQuadrado).apply(0));
+		
+		int resultado = aoQuadrado.compose(vezesDois).compose(maisDois).apply(0);
+		
+		System.out.println(resultado);
+	}
+
+}
