4a573a237119c5256f757993588e6487881d6161
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-02-09 18:08:24 -0300

    Java

--- banco_relacional/consultarEmpresas.sql
+++ banco_relacional/consultarEmpresas.sql
@@ -3,3 +3,7 @@ from empresas e, empresas_unidades eu, cidades c
 where e.id = eu.empresa_id 
 and c.id = eu.cidade_id
 and sede;
+
+-- select * from tbl_algo limit 4 offset 3;     limita a busca em 4 registros a partir do que foi especificado no offset
+--                                              limit 10 offset 0 trás os 10 primeiros registros
+--                                              limit 10 offset 10 trás do 11 ao 20 registro
--- jpa/src/infra/DAO.java
+++ jpa/src/infra/DAO.java
@@ -1,5 +1,71 @@
 package infra;
 
-public class DAO {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class DAO<T> {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Class<T> classe;
+	
+	static {
+		try {
+			emf = Persistence.createEntityManagerFactory("jpa");
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e);
+		}
+	}
+	
+	public DAO() {
+		this(null);
+	}
+	
+	public DAO(Class<T> classe) {
+		em = emf.createEntityManager();
+	}
+	
+	public DAO<T> abrirTrans() {
+		em.getTransaction().begin();
+		return this;
+	}
+	
+	public DAO<T> fecharTrans() {
+		em.getTransaction().commit();
+		return this;
+	}
+	
+	public DAO<T> incluir(T entidade) {
+		em.persist(entidade);
+		return this;
+	}
+	
+	public DAO<T> incluirAtomico(T entidade) {
+		return this.abrirTrans().incluir(entidade).fecharTrans();
+	}
+	
+	public List<T> obterTodos() {
+		return this.obterTodos(10, 0);
+	}
+	
+	public List<T> obterTodos(int qtd, int deslocamento) {
+		if (classe == null) {
+			throw new UnsupportedOperationException("Classe nula.");
+		}
+		
+		String jpql = "select e from " + classe.getName() + " e";
+		TypedQuery<T> query = em.createQuery(jpql, classe);
+		query.setMaxResults(qtd);
+		query.setFirstResult(deslocamento);
+		
+		return query.getResultList();
+	}
+	
+	public void fechar() {
+		em.close();
+	}
+	
 }
