2059c1d68767090ce13817068711d421dd9ea148
leandrobeandrade <leandrobe.andrade@gmail.com>
2023-02-14 13:07:10 -0300

    Java

--- fx/src/fx/layout/AppLayout.java
+++ fx/src/fx/layout/AppLayout.java
@@ -1,18 +1,31 @@
 package fx.layout;
 
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
 
-	public static void main(String[] args) {
-		
-	}
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		Parent raiz = null;
 		
+//		raiz = new TesteAnchorPane();
+//		raiz = new TesteBorderPane();
+//		raiz = new TesteFlowPane();
+//		raiz = new TesteGridPane();
+//		raiz = new TesteStackPane();
+		raiz = new TesteTilePane();		
+		
+		Scene principal = new Scene(raiz, 800, 600);		
+		primaryStage.setScene(principal);
+		primaryStage.setTitle("Gerenciadores de Layout");
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 }
